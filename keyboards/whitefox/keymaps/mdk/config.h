4cbc885f1d2df8b162bda204c15d6fe11e5b7974
Khoa Mai <khoa.mai@eficode.com>
2018-05-12 23:55:30 +0300

    debounce to 10

--- keyboards/whitefox/keymaps/mdk/config.h
+++ keyboards/whitefox/keymaps/mdk/config.h
@@ -1,7 +1,13 @@
+#ifdef  DEBOUNCE
+#undef  DEBOUNCE
+#define DEBOUNCE                   10
+#endif
+
 #define MOUSEKEY_DELAY             100
 #define MOUSEKEY_INTERVAL          40
 #define MOUSEKEY_MAX_SPEED         10
 #define MOUSEKEY_TIME_TO_MAX       16
 #define MOUSEKEY_WHEEL_MAX_SPEED   8
 #define MOUSEKEY_WHEEL_TIME_TO_MAX 40
+
 #define DISABLE_SPACE_CADET_ROLLOVER
